f8fa33d0a19a1578579ebd1e6ada2ec06e64aedd
granfass <74089629+granfass@users.noreply.github.com>
2020-11-24 20:28:22 +0900

    add stdout descriptions in kml file

--- shp-learning/main.py
+++ shp-learning/main.py
@@ -1,7 +1,21 @@
 #!/usr/bin/env python
 
+import xml.etree.ElementTree as etree
+from typing import Dict
+
+
 def main():
-    print('Hello world')
+    name_space: Dict[str, str] = {'kml': 'http://www.opengis.net/kml/2.2'}
+    name_space_uri: str = 'http://www.opengis.net/kml/2.2'
+
+    tree = etree.parse('kml/komae_bus.kml')
+    root = tree.getroot()
+
+    placemarks = root.findall(f"./{{{name_space_uri}}}Document/{{{name_space_uri}}}Placemark")
+    for i, placemark in enumerate(placemarks):
+        print(f"{i+1}番目の区間")
+        print(placemark.find('kml:description', name_space).text)
+
 
 if __name__ == "__main__":
     main()
