a4cda3841cf9e49a3d785d887649ce56668458b9
granfass <74089629+granfass@users.noreply.github.com>
2020-11-24 23:15:09 +0900

    add pytest for description

---
+++ shp_learning/main.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+import xml.etree.ElementTree as etree
+from typing import Dict, List
+
+NAME_SPACE: Dict[str, str] = {'kml': 'http://www.opengis.net/kml/2.2'}
+
+
+def get_description(element: etree.Element) -> (str, str):
+    busstops: List[str] = element.find('kml:description', NAME_SPACE).text.split()
+    return busstops[0], busstops[1]
+
+
+def main():
+    tree = etree.parse('kml/komae_bus.kml')
+    root = tree.getroot()
+
+    placemarks = root.findall(f"./{{{NAME_SPACE['kml']}}}Document/{{{NAME_SPACE['kml']}}}Placemark")
+    for i, placemark in enumerate(placemarks):
+        start_busstop, end_busstop = get_description(placemark)
+        print(f"{i+1}番目の区間")
+        print(placemark.find('kml:description', NAME_SPACE).text)
+
+        print(f"{i+1}番目のpoliline")
+        print(placemark.find('kml:LineString/kml:coordinates', NAME_SPACE).text)
+
+
+if __name__ == "__main__":
+    main()
--- tests/test_shp_learning.py
+++ tests/test_shp_learning.py
@@ -1,5 +1,33 @@
-from shp_learning import __version__
+import xml.etree.ElementTree as etree
+
+import pytest
+
+from shp_learning import main
 
 
-def test_version():
-    assert __version__ == '0.1.0'
+@pytest.fixture
+def create_xml() -> etree.Element:
+    xml_string: str = '<?xml version="1.0" encoding="UTF-8"?>' \
+        '<kml xmlns="http://www.opengis.net/kml/2.2">' \
+        '<Document>' \
+        '  <Placemark>' \
+        '    <name>1</name>' \
+        '    <description>狛江駅北口\n狛江市役所前</description>' \
+        '    <styleUrl>#m_ylw-pushpin</styleUrl>' \
+        '    <LineString>' \
+        '      <tessellate>1</tessellate>' \
+        '      <coordinates>' \
+        '            139.5773785398502,35.63317469234866,0 139.5783391581343,35.63395289893936,0 139.5783635194075,35.63400605957735,0' \
+        '      </coordinates>' \
+        '    </LineString>' \
+        '  </Placemark>' \
+        '</Document>' \
+        '</kml>'
+
+    element = etree.fromstring(xml_string)
+    yield element
+
+
+def test_get_description(create_xml):
+    element = create_xml.find(f"./{{{main.NAME_SPACE['kml']}}}Document/{{{main.NAME_SPACE['kml']}}}Placemark")
+    assert main.get_description(element) == ('狛江駅北口', '狛江市役所前')
